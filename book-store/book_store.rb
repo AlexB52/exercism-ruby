9012ffac3c23972ec96d5f34b7969ee40ae171d2
Alexandre Barret <barretalexandre@hotmail.com>
2020-10-05 21:57:02 +1300

    refactor code

--- book-store/book_store.rb
+++ book-store/book_store.rb
@@ -6,24 +6,28 @@ To get started with TDD, see the `README.md` file in your
 `ruby/book-store` directory.
 =end
 
+def price_of_basket(basket)
+  case basket.count
+  when 0
+    0.0
+  when 1
+    8.0
+  when 2
+    15.20
+  when 3
+    21.6
+  when 4
+    25.6
+  when 5
+    30
+  end
+end
+
 class BookStore
   def self.calculate_price(basket)
-    if basket.uniq == basket
-      case basket.count
-      when 0, 1
-        8.0 * basket.count
-      when 2
-        15.20
-      when 3
-        21.6
-      when 4
-        25.6
-      when 5
-        30
-      end
-    else
-      8.0 * basket.count
-    end
+    return price_of_basket(basket) if basket.uniq == basket
+
+    8.0 * basket.count
   end
 end
 
