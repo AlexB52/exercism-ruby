e1738cc7026613866be2babc7640c2be1945330e
Alexandre Barret <barretalexandre@hotmail.com>
2021-08-16 21:56:55 +1200

    Pass first test

--- grep/grep.rb
+++ grep/grep.rb
@@ -6,3 +6,25 @@ To get started with TDD, see the `README.md` file in your
 `ruby/grep` directory.
 =end
 
+module Grep
+  module_function
+
+  def grep(pattern, flags, files)
+    pattern = Regexp.new(pattern)
+    files.flat_map do |file|
+      File.new(file).grep(pattern)
+    end.first
+  end
+
+  class File
+    attr_reader :lines
+    def initialize(path)
+      @lines = ::File.read(path).split("\n")
+    end
+
+    def grep(pattern)
+      lines.select { |line| pattern =~ line }
+    end
+  end
+end
+
--- grep/grep_test.rb
+++ grep/grep_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require_relative 'grep'
-
+require 'byebug'
 # Common test data version: 1.2.0 4f2efaa
 class GrepTest < Minitest::Test
   def setup
