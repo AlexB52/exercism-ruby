08bbe5dcf30f0073ee33d43721d0fe3ba1294374
Alexandre Barret <alex@abletech.nz>
2021-10-24 11:05:16 +1300

    Solve Transpose exercise

---
+++ transpose/transpose.rb
@@ -0,0 +1,40 @@
+class Transpose
+  def self.transpose(input)
+    new(input).transpose
+  end
+
+  attr_reader :input, :line_length
+  def initialize(input)
+    @line_length = 1
+    @input = input
+  end
+
+  def transpose
+    return "" if input.empty?
+
+    columns.reverse
+           .map { |column| format(column) }
+           .reverse
+           .join("\n")
+  end
+
+  def rows
+    input.split("\n")
+         .map { |row| row.ljust(max_length).split("") }
+  end
+
+  def columns
+    rows.transpose
+  end
+
+  def max_length
+    @max_length ||= input.split("\n").map(&:length).max
+  end
+
+  def format(row)
+    result = row.join.rstrip
+    @line_length = result.length if result.length > line_length
+    result.ljust(line_length)
+  end
+end
+
