830181f8a166e07b9db32da0b2fe0c5017602ea3
Alexandre Barret <alex@abletech.nz>
2021-10-30 11:25:37 +1300

    Solve roman-numbers

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,7 +1,35 @@
-=begin
-Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
-`roman_numerals_test.rb` pass.
+# Based on https://sandimetz.com/blog/2016/6/9/make-everything-the-same
 
-To get started with TDD, see the `README.md` file in your
-`ruby/roman-numerals` directory.
-=end
+class Integer
+  ROMAN_MAP = {
+    1000 => "M",
+     500 => "D",
+     100 => "C",
+      50 => "L",
+      10 => "X",
+       5 => "V",
+       1 => "I",
+  }
+
+  SHORTER_MAP = {
+    "DCCCC" => "CM",
+    "CCCC"  => "CD",
+    "LXXXX" => "XC",
+    "XXXX"  => "XL",
+    "VIIII" => "IX",
+    "IIII"  => "IV",
+  }
+
+  def to_roman
+    result = ""
+    ROMAN_MAP.keys.reduce(self) do |number, roman_base|
+      quotient, remainder = number.divmod(roman_base)
+      result += ROMAN_MAP[roman_base] * quotient if quotient.positive?
+      remainder
+    end
+
+    SHORTER_MAP.reduce(result) do |result, (longer_form, shorter_form)|
+      result.gsub(longer_form, shorter_form)
+    end
+  end
+end
