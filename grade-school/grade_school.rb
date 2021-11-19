1b2467a1859d4a8e8a790a6aa9a1dfcd27b51807
Alexandre Barret <alex@abletech.nz>
2021-11-20 11:39:38 +1300

    Solve grade school

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -1,7 +1,42 @@
-=begin
-Write your code for the 'Grade School' exercise in this file. Make the tests in
-`grade_school_test.rb` pass.
+class Students
+  Student = Struct.new(:name, :grade)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/grade-school` directory.
-=end
+  def initialize
+    @collection = []
+  end
+
+  def for_grade(grade)
+    @collection.select { |student| student.grade == grade }
+  end
+
+  def add(student, grade)
+    @collection << Student.new(student, grade)
+    @collection.sort_by!(&:name)
+  end
+
+  def students_by_grade
+    @collection.sort_by(&:grade).group_by(&:grade)
+  end
+end
+
+class School
+  attr_reader :students_repository
+
+  def initialize(students: Students.new)
+    @students_repository = students
+  end
+
+  def students(grade)
+    students_repository.for_grade(grade).map(&:name)
+  end
+
+  def add(student, grade)
+    students_repository.add(student, grade)
+  end
+
+  def students_by_grade
+    students_repository.students_by_grade.map do |grade, students|
+      { grade: grade, students: students.map(&:name) }
+    end
+  end
+end
