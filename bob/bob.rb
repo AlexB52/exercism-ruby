361569ec24fa8f2475a28d02f1793eaf791d5cf1
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-13 19:09:33 +1200

    refactor bob

--- bob/bob.rb
+++ bob/bob.rb
@@ -2,12 +2,12 @@ module Bob
   module_function
 
   def hey(remark)
-    case (remark.split("\n").last || "").strip
-    when /^$/                        then "Fine. Be that way!"
-    when /^(?=.*[A-Z])[^a-z]+\?$/    then "Calm down, I know what I'm doing!"
-    when /^(?=.*[A-Z])[^a-z]+[^\?]$/ then "Whoa, chill out!"
-    when /\?$/                       then "Sure."
-    else                                  "Whatever."
+    case remark.gsub(/[^\w\?]/, "")
+    when /^(?=.*[A-Z])[^a-z]+\?$/ then "Calm down, I know what I'm doing!"
+    when /^(?=.*[A-Z])[^a-z]+$/   then "Whoa, chill out!"
+    when /\?$/                    then "Sure."
+    when /^$/                     then "Fine. Be that way!"
+    else                               "Whatever."
     end
   end
 end
