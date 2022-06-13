fd23eea793548e98047c98a558b9a3333c37dc90
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-13 18:37:30 +1200

    Solve Bob

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,7 +1,15 @@
-=begin
-Write your code for the 'Bob' exercise in this file. Make the tests in
-`bob_test.rb` pass.
+require "byebug"
 
-To get started with TDD, see the `README.md` file in your
-`ruby/bob` directory.
-=end
+module Bob
+  module_function
+
+  def hey(remark)
+    case (remark.split("\n").last || "").strip
+    when /^$/                        then "Fine. Be that way!"
+    when /^(?=.*[A-Z])[^a-z]+\?$/    then "Calm down, I know what I'm doing!"
+    when /^(?=.*[A-Z])[^a-z]+[^\?]$/ then "Whoa, chill out!"
+    when /\?$/                       then "Sure."
+    else                                  "Whatever."
+    end
+  end
+end
