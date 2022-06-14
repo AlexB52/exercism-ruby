67b751a3c868cc8cda28fbadc8925cd3f96b638b
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-14 17:12:36 +1200

    refactor anagram

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,9 +1,7 @@
 Anagram = Struct.new(:reference) do
   def match(words)
     words.select do |word|
-      a = reference.downcase
-      b = word.downcase
-
+      a, b = reference.downcase, word.downcase
       a != b && a.each_char.sort == b.each_char.sort
     end
   end
