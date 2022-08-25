d3e691bf21f1f33688111046a83abfc04c8d2f36
Alexandre Barret <alexandre@hatchinvest.nz>
2022-08-25 21:15:11 +1200

    Solve change

--- change/change.rb
+++ change/change.rb
@@ -1,15 +1,39 @@
 class Change
-  def self.generate(coins, change)
-    return [change] if coins.include?(change)
+  class ImpossibleCombinationError < StandardError ;end
+  class NegativeTargetError < StandardError ;end
 
-    result = []
-    coins.sort.reverse.each do |coin|
-      next if coin > change
-      number, rest = change.divmod(coin)
-      result.concat(Array.new(number) { coin })
-      change = rest
-      break if change.zero?
+  def self.generate(coins, change)
+    if change < 0
+      raise NegativeTargetError
     end
-    result.sort
+
+    unless (result = self.ways(coins, change)[change])
+      raise ImpossibleCombinationError
+    end
+
+    result
+  end
+
+  def self.ways(coins, max)
+    (1..max).each.with_object({ 0 => [] }) do |i, result|
+      coins.each do |coin|
+        target = i
+        combination = []
+
+        until target <= 0
+          target -= coin
+          combination << coin
+          if result[target]
+            combination.concat(result[target])
+            break
+          end
+        end
+
+        if combination.sum == i
+          result[i] ||= combination
+          result[i] = [result[i], combination].min_by(&:length)
+        end
+      end
+    end
   end
 end
