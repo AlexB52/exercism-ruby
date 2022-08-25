67e4f56566064ead69b18af5e20cab9d419b603f
Alexandre Barret <alexandre@hatchinvest.nz>
2022-08-25 22:01:24 +1200

    Refactor change solution

--- change/change.rb
+++ change/change.rb
@@ -7,14 +7,14 @@ class Change
       raise NegativeTargetError
     end
 
-    unless (result = self.ways(coins, change)[change])
+    unless (result = self.coins_per_change(coins, change)[change])
       raise ImpossibleCombinationError
     end
 
     result
   end
 
-  def self.ways(coins, max)
+  def self.coins_per_change(coins, max)
     (1..max).each.with_object({ 0 => [] }) do |i, result|
       coins.each do |coin|
         target = i
@@ -25,11 +25,11 @@ class Change
           combination << coin
           if result[target]
             combination.concat(result[target])
-            break
+            target = 0
           end
         end
 
-        if combination.sum == i
+        if target == 0
           result[i] ||= combination
           result[i] = [result[i], combination].min_by(&:length)
         end
