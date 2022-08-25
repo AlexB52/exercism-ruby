99891967dcb18208494a1d391682269de15808cd
Alexandre Barret <alexandre@hatchinvest.nz>
2022-08-26 10:00:53 +1200

    Refactor solution once more from experiments

--- change/change.rb
+++ change/change.rb
@@ -7,55 +7,18 @@ class Change
       raise NegativeTargetError
     end
 
-    unless (result = self.coins_per_change3(coins, change)[change])
-      raise ImpossibleCombinationError
-    end
-
-    result
-  end
-
-  def self.coins_per_change(coins, max)
-    (1..max).each.with_object({ 0 => [] }) do |i, result|
-      coins.each do |coin|
-        target = i-coin
-        if result[target]
-          combination = [coin].concat(result[target])
-          result[i] ||= combination
-          result[i] = [result[i], combination].min_by(&:length)
-        end
-      end
-    end
-  end
-
-  def self.coins_per_change2(coins, max)
-    (1..max).each.with_object({ 0 => [] }) do |i, result|
+    coins_per_change = (1..change).each.with_object({ 0 => [] }) do |i, result|
       result[i] = coins
         .map { |coin| [coin].concat(result[i-coin] || []) }
         .select { _1.sum == i }
         .min_by(&:length)
     end
-  end
 
-  def self.coins_per_change3(coins, max)
-    Hash.new do |h, k|
-      if k <= 0
-        h[0] = []
-      else
-        h[k] = coins
-          .map { |coin| [coin].concat(h[k-coin] || []) }
-          .select { _1.sum == k }
-          .min_by(&:length)
-      end
+    unless (result = coins_per_change[change])
+      raise ImpossibleCombinationError
     end
+
+    result
   end
-end
-
-require 'benchmark'
-
-n = 50
-Benchmark.bm do |x|
-  x.report("sol 1") { n.times { Change.coins_per_change([1, 2, 5, 10, 20, 50, 100],1999) } }
-  x.report("sol 2") { n.times { Change.coins_per_change2([1, 2, 5, 10, 20, 50, 100],1999) } }
-  x.report("sol 3") { n.times { Change.coins_per_change3([1, 2, 5, 10, 20, 50, 100],1999)[1999] } }
 end
 
--- change/test.rb
+++ change/test.rb
@@ -1,65 +1,61 @@
-require "byebug"
 class Change
+  class ImpossibleCombinationError < StandardError ;end
+  class NegativeTargetError < StandardError ;end
+
+  def self.generate(coins, change)
+    if change < 0
+      raise NegativeTargetError
+    end
+
+    unless (result = self.coins_per_change3(coins, change)[change])
+      raise ImpossibleCombinationError
+    end
+
+    result
+  end
+
   def self.coins_per_change(coins, max)
-    result = Hash.new do |h, k|
+    (1..max).each.with_object({ 0 => [] }) do |i, result|
+      coins.each do |coin|
+        target = i-coin
+        if result[target]
+          combination = [coin].concat(result[target])
+          result[i] ||= combination
+          result[i] = [result[i], combination].min_by(&:length)
+        end
+      end
+    end
+  end
+
+  def self.coins_per_change2(coins, max)
+    (1..max).each.with_object({ 0 => [] }) do |i, result|
+      result[i] = coins
+        .map { |coin| [coin].concat(result[i-coin] || []) }
+        .select { _1.sum == i }
+        .min_by(&:length)
+    end
+  end
+
+  def self.coins_per_change3(coins, max)
+    Hash.new do |h, k|
       if k <= 0
         h[0] = []
-      elsif coins.include?(k)
-        h[k] = [k]
       else
         h[k] = coins
-          .filter_map { |coin| [coin].concat(h[k-coin]) }
+          .map { |coin| [coin].concat(h[k-coin] || []) }
           .select { _1.sum == k }
           .min_by(&:length)
       end
     end
-
-    result[max]
-    result
   end
 end
 
-require "minitest/autorun"
+require 'benchmark'
 
-class TestChange < MiniTest::Test
-  def test_coins_per_change
-    expected = {
-      0=>[],
-      1=>[1],
-      2=>[2],
-      3=>[1, 2],
-      4=>[4],
-      5=>[1, 4],
-      6=>[2, 4],
-      7=>[1, 2, 4],
-      8=>[8],
-      9=>[1, 8],
-      10=>[2, 8]
-    }
-
-    assert_equal(expected, Change.coins_per_change([1, 2, 4, 8], 10))
-  end
+n = 50
+Benchmark.bm do |x|
+  x.report("sol 1") { n.times { Change.coins_per_change([1, 2, 5, 10, 20, 50, 100],1999) } }
+  x.report("sol 2") { n.times { Change.coins_per_change2([1, 2, 5, 10, 20, 50, 100],1999) } }
+  x.report("sol 3") { n.times { Change.coins_per_change3([1, 2, 5, 10, 20, 50, 100],1999)[1999] } }
 end
 
-  # def self.coins_per_change(coins, max)
-  #   (1..max).each.with_object({ 0 => [] }) do |i, result|
-  #     coins.each do |coin|
-  #       target = i
-  #       combination = []
-
-  #       until target <= 0
-  #         target -= coin
-  #         combination << coin
-  #         if result[target]
-  #           combination.concat(result[target])
-  #           target = 0
-  #         end
-  #       end
-
-  #       if target == 0
-  #         result[i] ||= combination
-  #         result[i] = [result[i], combination].min_by(&:length)
-  #       end
-  #     end
-  #   end
-  # end
