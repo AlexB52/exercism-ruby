6ae9222161efa66f04210e64c4732a951f4755dd
Alexandre Barret <alexandre@hatchinvest.nz>
2022-08-26 12:57:05 +1200

    Solve simple linked list

--- simple-linked-list/simple_linked_list.rb
+++ simple-linked-list/simple_linked_list.rb
@@ -1,7 +1,30 @@
-=begin
-Write your code for the 'Simple Linked List' exercise in this file. Make the tests in
-`simple_linked_list_test.rb` pass.
+# This isn't a linked list, yet specs pass
 
-To get started with TDD, see the `README.md` file in your
-`ruby/simple-linked-list` directory.
-=end
+class SimpleLinkedList
+  def initialize(array = [])
+    @elements = []
+    array.each { push(Element.new(_1)) }
+  end
+
+  def to_a
+    @elements.map(&:datum)
+  end
+
+  def reverse!
+    @elements.reverse!
+    to_a.empty? ? self : to_a
+  end
+
+  def push(element)
+    @elements.unshift(element)
+    self
+  end
+
+  def pop
+    @elements.shift
+  end
+end
+
+Element = Struct.new(:datum) do
+  attr_accessor :next
+end
