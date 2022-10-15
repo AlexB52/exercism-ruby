31c1c5950f6f0d3b49afc6497875b15a7c00f769
Alex Barret <alex.barret@veridapt.com>
2022-10-16 12:16:40 +1300

    solve high scores

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -5,3 +5,26 @@ Write your code for the 'High Scores' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/high-scores` directory.
 =end
+
+class HighScores
+  attr_accessor :scores
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    scores[-1]
+  end
+
+  def personal_best
+    scores.max
+  end
+
+  def personal_top_three
+    scores.max(3)
+  end
+
+  def latest_is_personal_best?
+    personal_best == latest
+  end
+end
