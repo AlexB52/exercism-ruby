ab2b872ffe0a505763b0bbabf7fa8194455b034d
Alex Barret <alex.barret@veridapt.com>
2022-10-16 16:07:41 +1300

    refactor luhn

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,30 +1,20 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
-
 module Luhn
   module_function
   def valid?(input)
-    return false unless input.match?(/^[\d][\s\d]+$/)
+    return false unless input.match?(/^\d[\s\d]+$/)
 
-    checksum = 0
     input.scan(/\d/)
-         .reverse
-         .each
-         .with_index do |digit, i|
-        value = digit.to_i
+         .reverse_each
+         .with_index
+         .sum do |digit, i|
+            value = digit.to_i
 
-        if i % 2 != 0
-          value *= 2
-          value -= 9 if value > 9
-        end
-        checksum += value
-      end
+            if i % 2 != 0
+              value *= 2
+              value -= 9 if value > 9
+            end
 
-    checksum % 10 == 0
+            value
+          end % 10 == 0
   end
 end
