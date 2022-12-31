756ec34ff7a2cb68ffb0585d97c9c16fccf71d91
Alexandre Barret <barret.alx@gmail.com>
2022-12-31 19:21:42 +1300

    Refactor proverb

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -1,16 +1,19 @@
 class Proverb
   def initialize(*things, qualifier: nil)
     @things = things
-    @goal = things.first
-    if qualifier
-      @goal = format("%s %s", qualifier, @goal)
-    end
+    @qualifier = qualifier
   end
 
   def to_s
     @things.each_cons(2)
            .map { |want, lost| format("For want of a %s the %s was lost.", want, lost) }
-           .push(format("And all for the want of a %s.", @goal))
+           .push(format("And all for the want of a %s.", goal))
            .join("\n")
+  end
+
+  private
+
+  def goal
+    "#{@qualifier} #{@things.first}".strip
   end
 end
